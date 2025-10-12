6f1e713edb2f1cf4cda440833d575039f03e0086
Ayoub Alawad <109764757+ayobawd@users.noreply.github.com>
2025-10-12 12:06:49 +0000

    a

---
+++ test.sh
@@ -0,0 +1,5 @@
+while true; do
+  # Commands to execute repeatedly
+  echo "This loop runs forever (or until interrupted)."
+  sleep 1 # Optional: pause for 1 second to prevent excessive resource usage
+done
